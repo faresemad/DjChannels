3e28a5f27d80177e1f70fe2ba707b7aac4bd2f38
Fares Emad <faresemadx@gmail.com>
2023-07-07 20:55:54 +0300

    feat: create chat app and add the index view

--- project/settings.py
+++ project/settings.py
@@ -19,14 +19,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 3nd Package
+    "daphne",
+    # Django Apps
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # 3nd Package
-    "daphne",
+    # Local Apps
+    "chat",
 ]
 
 MIDDLEWARE = [
